a099792cfb71434892e14bf4913b01e86e97b769
Hjalte Sorgenfrei Mac Dalland <hda@cvation.com>
2022-11-29 13:51:21 +0100

    Add colours

--- main.cpp
+++ main.cpp
@@ -15,30 +15,37 @@ union Colour {
     bool gray_scale_2() {
         return components.red | components.green | components.blue;
     }
+
+    Colour(uint32_t colour) : colour{colour} {}
 };
 
-void set_pixel(int x, int y, int colour) {
-    image->set_pixel(x, y, colour, colour, colour, 255);
+namespace Colours {
+    Colour White = Colour(0xFFFFFFFF);
+    Colour Black = Colour(0x00000000);
 }
 
-void x_line(int x1, int x2, int y, int colour)
+void set_pixel(uint32_t x, uint32_t y, Colour colour) {
+    image->set_pixel(x, y, colour.components.red, colour.components.green, colour.components.blue, 255);
+}
+
+void x_line(uint32_t x1, uint32_t x2, uint32_t y, Colour colour)
 {
     while (x1 <= x2) set_pixel(x1++, y, colour);
 }
 
-void y_line(int x, int y1, int y2, int colour)
+void y_line(uint32_t x, uint32_t y1, uint32_t y2, Colour colour)
 {
     while (y1 <= y2) set_pixel(x, y1++, colour);
 }
 
 // (uint32_t x, uint32t_t y, uint32t_t radius, uint32t_t width, Pixel pixel) is probably a better interface
-void plot_between_circles(int xc, int yc, int inner, int outer, int colour)
+void plot_between_circles(uint32_t xc, uint32_t yc, uint32_t inner, uint32_t outer, Colour colour)
 {
-    int xo = outer;
-    int xi = inner;
-    int y = 0;
-    int erro = 1 - xo;
-    int erri = 1 - xi;
+    uint32_t xo = outer;
+    uint32_t xi = inner;
+    uint32_t y = 0;
+    int32_t erro = 1 - xo;
+    int32_t erri = 1 - xi;
 
     while(xo >= y) {
         x_line(xc + xi, xc + xo, yc + y,  colour);
@@ -72,10 +79,10 @@ void plot_between_circles(int xc, int yc, int inner, int outer, int colour)
     }
 }
 
-void clear_image(int clearColour = 255) {
-    for (int x = 0; x < image->bmp_info_header.width; x++) {
-        for (int y = 0; y < image->bmp_info_header.height; y++) {
-            set_pixel(x, y, clearColour);
+void clear_image(Colour colour = Colours::White) {
+    for (uint32_t x = 0; x < image->bmp_info_header.width; x++) {
+        for (uint32_t y = 0; y < image->bmp_info_header.height; y++) {
+            set_pixel(x, y, colour);
         }
     }
 }
@@ -85,7 +92,7 @@ void clear_image(int clearColour = 255) {
 int main() {
     image = new BMP(800, 600, false);
     clear_image();
-    plot_between_circles(400, 300, 100, 123, 0);
+    plot_between_circles(400, 300, 100, 123, Colours::Black);
     image->write("circle.bmp");
     delete image;
     return 0;
