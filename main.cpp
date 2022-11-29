7bb4a35ccfb1df93c9058bcfa9a1b669cfc05dbb
Hjalte Sorgenfrei Mac Dalland <hda@cvation.com>
2022-11-29 13:41:29 +0100

    creating colour struct

--- main.cpp
+++ main.cpp
@@ -3,6 +3,20 @@
 
 BMP* image;
 
+union Colour {
+    struct Components {
+        uint8_t red;
+        uint8_t green;
+        uint8_t blue;
+        uint8_t alpha; // Unused
+    } components; 
+    uint32_t colour;
+
+    bool gray_scale_2() {
+        return components.red | components.green | components.blue;
+    }
+};
+
 void set_pixel(int x, int y, int colour) {
     image->set_pixel(x, y, colour, colour, colour, 255);
 }
@@ -17,6 +31,7 @@ void y_line(int x, int y1, int y2, int colour)
     while (y1 <= y2) set_pixel(x, y1++, colour);
 }
 
+// (uint32_t x, uint32t_t y, uint32t_t radius, uint32t_t width, Pixel pixel) is probably a better interface
 void plot_between_circles(int xc, int yc, int inner, int outer, int colour)
 {
     int xo = outer;
@@ -57,7 +72,7 @@ void plot_between_circles(int xc, int yc, int inner, int outer, int colour)
     }
 }
 
-void clearImage(int clearColour = 255) {
+void clear_image(int clearColour = 255) {
     for (int x = 0; x < image->bmp_info_header.width; x++) {
         for (int y = 0; y < image->bmp_info_header.height; y++) {
             set_pixel(x, y, clearColour);
@@ -69,7 +84,7 @@ void clearImage(int clearColour = 255) {
 
 int main() {
     image = new BMP(800, 600, false);
-    clearImage();
+    clear_image();
     plot_between_circles(400, 300, 100, 123, 0);
     image->write("circle.bmp");
     delete image;
