351bf6ca2341ef08bdbaba798d5d6fbc51484a48
Hjalte Sorgenfrei Mac Dalland <hjaltesorgenfrei@gmail.com>
2022-12-05 21:34:31 +0100

    Version and libsdl

--- make.sh
+++ make.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-g++ main.cpp -o indtryk.exe -lSDL2 -lSDL2_gfx
-emcc --bind main.cpp -o indtryk.html -s USE_SDL=2 -s USE_SDL_GFX=2 -s ALLOW_MEMORY_GROWTH=1
+g++ main.cpp -o indtryk.exe -lSDL2
+emcc --bind main.cpp -o indtryk.html -s USE_SDL=2 -s ALLOW_MEMORY_GROWTH=1
